ace6731be399cfe3c942007f1f6ca6d81e8d76d5
AmauryD19 <amaurydotrice@gmail.com>
2019-03-18 19:29:34 +0100

    Try var into let
    Fonctionne pour le premier graph mais pas quand j'ajoute le second.

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -1,7 +1,7 @@
 /* 
 // WRITE YOUR JAVASCRIPT BELOW THIS COMMENT 
 Your name : Dotrice Amaury 
-Date :  
+Date : 19/03/2018
 Contact information : 
 What does this script do ? 
 ...
@@ -10,40 +10,40 @@ What does this script do ?
 // Your scripting goes here...
 /* Premier graph */
 
-/* Création de la div tabdiv */
-var div = document.createElement("div");
+/* Création du bloc du premier graph */
+var div = document.createElement("div"); //Création et placement de la div contenant le premier tableau
 div.id = "tabdiv";
 var container = document.getElementById("mw-content-text");
-
 container.insertBefore(div, table1);
 
 /* Les variables */
-var data = [];
+var data = []; //Array contenant les données
 var table = document.getElementById("table1");
 var years = table.getElementsByTagName("tr")[1].getElementsByTagName("th");
-var yearsArray = [];
-
-
-for (let i = 1; i < years.length; i++) {
-    let content = years[i].innerHTML;
-    yearsArray.push(content);
-}
-
+var yearsArray = []; //Array contenant les années
 var rows = table.getElementsByTagName("tr");
 
-for (let i = 1; i < rows.length; i++) {
-    let cells = rows[i].getElementsByTagName("td");
+/* Données du premier graph */
+for (let i = 1; i < years.length; i++) { //Ici je push les années dans l'array
+    let allYears = years[i].innerHTML;
+    yearsArray.push(allYears);
+}
 
-    for (let j = 0; j < cells.length; j++) {
+for (let i = 1; i < rows.length; i++) { //Ici push les données dans 
+    let allData = rows[i].getElementsByTagName("td");
+
+    for (let j = 0; j < allData.length; j++) { //Ici si j est 0 la variable pays sera le nom du pays
         if (j === 0) {
-            var pays = cells[j].innerHTML;
-        } else if (!isNaN(parseInt(cells[j].innerHTML))) {
-            data.push({ data: parseInt(cells[j].innerHTML), pays: pays, years: yearsArray[j - 1] });
+            var pays = allData[j].innerHTML;
+        } else if (!isNaN(parseInt(allData[j].innerHTML))) { //Ici si j n'est pas 0 on push un objet dans l'array Data contenant toutes les informations
+            data.push({ data: parseInt(allData[j].innerHTML), pays: pays, years: yearsArray[j - 1] });
         }
-
     }
 }
 
+/* Création du second graph */
+
+/* Création du bloc du second graph */
 var svg = dimple.newSvg("#tabdiv", "100%", 450);
 var myChart = new dimple.chart(svg, data);
 myChart.setBounds(30, 110, "90%", 305);
@@ -51,43 +51,43 @@ var x = myChart.addCategoryAxis("x", ["years", "pays"]);
 var y = myChart.addMeasureAxis("y", "data");
 y.ticks = 20;
 myChart.addSeries("pays", dimple.plot.line);
-myChart.addSeries("pays", dimple.plot.scatter);
+myChart.addSeries("pays", dimple.plot.bubble);
 myChart.addLegend(10, 10, "100%", 200);
 myChart.draw();
 
-/* Deuxième graph */
+/* Deuxième graph : même démarche que pour le premier graph */
 
 var div = document.createElement("div");
 div.id = "tabdiv2";
 var container = document.getElementById("mw-content-text");
-
 container.insertBefore(div, table2);
 
+/* Les variables */
 var data = [];
 var table = document.getElementById("table2");
 var years = table.getElementsByTagName("tr")[0].getElementsByTagName("th");
 var yearsArray = [];
-
-for (let i = 1; i < years.length; i++) {
-    let content = years[i].innerHTML;
-    yearsArray.push(content);
-}
-
 var rows = table.getElementsByTagName("tr");
 
+/* Donnée du second graph*/
+for (let i = 1; i < years.length; i++) {
+    let allYears = years[i].innerHTML;
+    yearsArray.push(allYears);
+}
+
 for (let i = 1; i < rows.length; i++) {
-    let cells = rows[i].getElementsByTagName("td");
+    let allData = rows[i].getElementsByTagName("td");
 
-    for (let j = 0; j < cells.length; j++) {
+    for (let j = 0; j < allData.length; j++) {
         if (j === 0) {
-            var pays = cells[j].innerHTML;
-        } else if (!isNaN(parseInt(cells[j].innerHTML))) {
-            data.push({ data: parseInt(cells[j].innerHTML), pays: pays, years: yearsArray[j - 1] });
+            var pays = allData[j].innerHTML;
+        } else if (!isNaN(parseInt(allData[j].innerHTML))) {
+            data.push({ data: parseInt(allData[j].innerHTML), pays: pays, years: yearsArray[j - 1] });
         }
-
     }
 }
 
+/* Création du second graph */
 var myChart = new dimple.chart(dimple.newSvg("#tabdiv2", "100%", 550), data);
 myChart.setBounds(35, 180, "90%", 305);
 var x = myChart.addCategoryAxis("x", ["years", "pays"]);
@@ -95,6 +95,10 @@ x.addOrderRule("years", false);
 var y = myChart.addMeasureAxis("y", "data");
 y.ticks = 15;
 myChart.addSeries("pays", dimple.plot.bubble);
-myChart.addSeries("pays", dimple.plot.scatter);
 myChart.addLegend(10, 10, "100%", 200);
 myChart.draw();
+
+/* Je n'ai pas réussi à implémenter le second graphique tout en gardant des "let".
+De base je n'utilisais "var" que pour la pays, dans la boucle j === 0, car cela ne fonctionnait pas autrement et je n'ai pas trouvé de solution autre que l'utilisation de "var".
+Quand j'utilisais "let", tout fonctionne parfaitement pour le premier graphique, mais quand j'ajoute le second rien ne va plus. J'ai essayé de changer le nom des variables
+mais cela n'a rien changé, probablement une faute d'inattention venant de ma part, mais je n'arrive pas à mettre le doigt dessus je suis donc resté sur des "var". */
