ca20571ad4f410109ed7eedc94dedcdf0c95702a
AmauryD19 <amaurydotrice@gmail.com>
2019-03-19 10:28:12 +0100

    Suppression var + commentaires

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -11,17 +11,17 @@ What does this script do ?
 /* Premier graph */
 
 /* Création du bloc du premier graph */
-var div = document.createElement("div"); //Création et placement de la div contenant le premier tableau
+div = document.createElement("div"); //Création et placement de la div contenant le premier tableau
 div.id = "tabdiv";
-var container = document.getElementById("mw-content-text");
+container = document.getElementById("mw-content-text");
 container.insertBefore(div, table1);
 
-/* Les variables */
-var data = []; //Array contenant les données
-var table = document.getElementById("table1");
-var years = table.getElementsByTagName("tr")[1].getElementsByTagName("th");
-var yearsArray = []; //Array contenant les années
-var rows = table.getElementsByTagName("tr");
+/* Les ables */
+data = []; //Array contenant les données
+table = document.getElementById("table1");
+years = table.getElementsByTagName("tr")[1].getElementsByTagName("th");
+yearsArray = []; //Array contenant les années
+rows = table.getElementsByTagName("tr");
 
 /* Données du premier graph */
 for (i = 1; i < years.length; i++) { //Ici je push les années dans l'array
@@ -32,44 +32,39 @@ for (i = 1; i < years.length; i++) { //Ici je push les années dans l'array
 for (i = 1; i < rows.length; i++) { //Ici push les données dans 
     allData = rows[i].getElementsByTagName("td");
 
-    for (j = 0; j < allData.length; j++) { //Ici si j est 0 la variable pays sera le nom du pays
+    for (j = 0; j < allData.length; j++) { //Ici si j est 0 la varable pays sera le nom du pays
         if (j === 0) {
-            var pays = allData[j].innerHTML;
+            pays = allData[j].innerHTML;
         } else if (!isNaN(parseInt(allData[j].innerHTML))) { //Ici si j n'est pas 0 on push un objet dans l'array Data contenant toutes les informations
             data.push({ data: parseInt(allData[j].innerHTML), pays: pays, years: yearsArray[j - 1] });
         }
     }
 }
 
-/* Création du second graph */
+/* Création du second graph, même démarche que pour le premier */
 
-/* Création du bloc du second graph */
-var svg = dimple.newSvg("#tabdiv", "100%", 450);
-var myChart = new dimple.chart(svg, data);
+svg = dimple.newSvg("#tabdiv", "100%", 450);
+myChart = new dimple.chart(svg, data);
 myChart.setBounds(30, 110, "90%", 305);
-var x = myChart.addCategoryAxis("x", ["years", "pays"]);
-var y = myChart.addMeasureAxis("y", "data");
+x = myChart.addCategoryAxis("x", ["years", "pays"]);
+y = myChart.addMeasureAxis("y", "data");
 y.ticks = 20;
 myChart.addSeries("pays", dimple.plot.line);
 myChart.addSeries("pays", dimple.plot.bubble);
 myChart.addLegend(10, 10, "100%", 200);
 myChart.draw();
 
-/* Deuxième graph : même démarche que pour le premier graph */
-
-var div = document.createElement("div");
+div = document.createElement("div");
 div.id = "tabdiv2";
-var container = document.getElementById("mw-content-text");
+container = document.getElementById("mw-content-text");
 container.insertBefore(div, table2);
 
-/* Les variables */
-var data = [];
-var table = document.getElementById("table2");
-var years = table.getElementsByTagName("tr")[0].getElementsByTagName("th");
-var yearsArray = [];
-var rows = table.getElementsByTagName("tr");
+data = [];
+table = document.getElementById("table2");
+years = table.getElementsByTagName("tr")[0].getElementsByTagName("th");
+yearsArray = [];
+rows = table.getElementsByTagName("tr");
 
-/* Donnée du second graph*/
 for (i = 1; i < years.length; i++) {
     allYears = years[i].innerHTML;
     yearsArray.push(allYears);
@@ -80,24 +75,23 @@ for (i = 1; i < rows.length; i++) {
 
     for (j = 0; j < allData.length; j++) {
         if (j === 0) {
-            var pays = allData[j].innerHTML;
+            pays = allData[j].innerHTML;
         } else if (!isNaN(parseInt(allData[j].innerHTML))) {
             data.push({ data: parseInt(allData[j].innerHTML), pays: pays, years: yearsArray[j - 1] });
         }
     }
 }
 
-/* Création du second graph */
-var svg = dimple.newSvg("#tabdiv2", "100%", 550);
-var myChart = new dimple.chart(svg, data);
+svg = dimple.newSvg("#tabdiv2", "100%", 550);
+myChart = new dimple.chart(svg, data);
 myChart.setBounds(35, 180, "90%", 305);
-var x = myChart.addCategoryAxis("x", ["years", "pays"]);
-var y = myChart.addMeasureAxis("y", "data");
+x = myChart.addCategoryAxis("x", ["years", "pays"]);
+y = myChart.addMeasureAxis("y", "data");
 y.ticks = 15;
 myChart.addSeries("pays", dimple.plot.bubble);
 myChart.addLegend(10, 10, "100%", 200);
 myChart.draw();
-/* Je n'ai pas réussi à implémenter le second graphique tout en gardant des ".
-De base je n'utilisais "var" que pour la pays, dans la boucle j === 0, car cela ne fonctionnait pas autrement et je n'ai pas trouvé de solution autre que l'utilisation de "var".
-Quand j'utilisais ", tout fonctionne parfaitement pour le premier graphique, mais quand j'ajoute le second rien ne va plus. J'ai essayé de changer le nom des variables
-mais cela n'a rien changé, probablement une faute d'inattention venant de ma part, mais je n'arrive pas à mettre le doigt dessus je suis donc resté sur des "var". */
+/* Je n'ai pas réussi à implémenter le second graphique tout en utilisant des "let". Pour la variable years je suis obligé de passer de base par "var"
+et non par "let" car sinon la variable pays n'est pas définie car probablement un conflit avec la première variable pays. Probablement un soucis
+de nomenclature de ma part. J'ai donc supprimé les "var" et ça fonctionne, mais si je passe en "let" rien ne s'affiche. Même quand je corrige les
+erreurs affichée dans la console rien ne fonctionne. J'ai supprimé les console.log une fois que tout était réglé.*/
